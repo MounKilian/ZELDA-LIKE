448fd04cd623f78dde98bcbdb67e5356cc66ce1b
Kilian <144996592+MounKilian@users.noreply.github.com>
2026-02-10 23:45:59 +0100

    Ennemy can walk alone and detect player

--- ZELDA/Assets/Scripts/Ennemy.cs
+++ ZELDA/Assets/Scripts/Ennemy.cs
@@ -1,18 +1,27 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.U2D;
 
 public class Ennemy : MonoBehaviour
 {
+    [Header("Référence")]
     public SO_Ennemy stats;
     public Rigidbody2D rb;
     public SpriteRenderer sprite;
+    public GameObject player;
 
+    [Header("Settings")]
     public Vector2 dir;
-
     private float currentHealth;
+    public float detectionRange = 5f;
+    public int walk = 0;
+    private bool canFollow = false;
+    private bool isTouchingPlayer = false;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        dir = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
         currentHealth = stats.health;
     }
 
@@ -38,9 +47,50 @@ public class Ennemy : MonoBehaviour
 
     void FollowPlayer()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        dir = (player.transform.position - transform.position).normalized;
+        float distance = Vector2.Distance(player.transform.position, transform.position);
+
+        if (isTouchingPlayer)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
+        if (distance <= detectionRange)
+        {
+            canFollow = true;
+        }
+
+        if (!canFollow)
+        {
+            if (walk > 100)
+            {
+                dir = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+                walk = 0;
+            }
+            walk++;
+        } 
+        else
+        {
+            dir = (player.transform.position - transform.position).normalized;
+        };
 
         rb.linearVelocity = dir * stats.speed * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isTouchingPlayer = true;
+            Debug.Log("Ghost hit");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isTouchingPlayer = false;
+        }
     }
 }
